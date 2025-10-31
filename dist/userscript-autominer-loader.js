c11de09345e30be4af123a017dccb7a4bab9cbdd
Giuseppe Giulio Caponetto <giuseppe.caponetto@geoimpact.ch>
2025-11-01 00:28:18 +0100

    fixed url to dist miner

--- dist/userscript-autominer-loader.js
+++ dist/userscript-autominer-loader.js
@@ -1,7 +1,7 @@
 "use strict";
 (() => {
     // const autominerUrl = `http://localhost:3000/autominer.js?t=${Date.now()}`;
-    const autominerUrl = `https://raw.githubusercontent.com/ggcaponetto/midnight-autominer/refs/heads/main/autominer.js?t=${Date.now()}`;
+    const autominerUrl = `https://raw.githubusercontent.com/ggcaponetto/midnight-autominer/refs/heads/main/dist/autominer.js?t=${Date.now()}`;
     return fetch(autominerUrl)
         .then((response) => response.text())
         .then((code) => {
